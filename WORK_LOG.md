bcdd0a00c50db4f6351c9b9722b5a2af794a1437
Sohum Berry <sohumberry@gmail.com>
2025-02-24 15:03:05 -0800

    First Log

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -1,9 +1,8 @@
-Name: YOUR NAME HERE
+Name: Sohum Berry
 
-| Date    |  Time  |                                     Update |
-|:--------|:------:|-------------------------------------------:|
-| April 5 | 9-10pm | I tried to write abstract, efficient code. |
-|         |        |                                            |
+| Date    |   Time    |                                                                                                                                                                                             Update |
+|:--------|:---------:|---------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------:|
+| 2/24/25 | 2:30-3:00 | I spent some time brainstorming and made a plan on the board. I just spent the last 20 or so minutes writing the corresponding code. It's looking good in my head, but I have no clue how it runs. |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -3,7 +3,7 @@
  * A tool for finding the longest shared substring between two documents.
  *
  * @author Zach Blick
- * @author YOUR NAME HERE
+ * @author Sohum Berry
  */
 public class PlagiarismChecker {
 
@@ -15,9 +15,26 @@ public class PlagiarismChecker {
      * @return The length of the longest shared substring.
      */
     public static int longestSharedSubstring(String doc1, String doc2) {
-
-        // TODO Complete this function to return the length of the longest shared substring.
+        // Storing the lengths of the indices connecting text one to text two
+        int[][] memoizationBoard = new int[doc1.length()][doc2.length()];
 
         return 0;
+    }
+
+    public static int recursive(String text1, String text2, int idx1, int idx2, int[][] memoizationBoard) {
+        if (idx1 >= text1.length() || idx2 >= text2.length()) {
+            return 0;
+        }
+
+        int maxLen = Integer.MIN_VALUE;
+        for (int i = idx1; i < text1.length(); i++) {
+            if (text1.charAt(i) == text2.charAt(idx2)) {
+                int newLen = 1+recursive(text1, text2, i+1, idx2+1, memoizationBoard);
+                memoizationBoard[i][idx2] = newLen;
+                maxLen = Math.max(maxLen, newLen);
+            }
+        }
+
+        return maxLen;
     }
 }
