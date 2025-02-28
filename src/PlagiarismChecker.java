515f1462eb8f4a5bf1341fc7d69cf340a42b13da
Sohum Berry <sohumberry@gmail.com>
2025-02-28 13:22:04 -0800

    Sixth Log

--- src/PlagiarismChecker.java
+++ src/PlagiarismChecker.java
@@ -19,36 +19,22 @@ public class PlagiarismChecker {
         int[][] board = new int[doc1.length()+1][doc2.length()+1];
         for (int i = 1; i < doc1.length()+1; i++) {
             for (int j = 1; j < doc2.length()+1; j++) {
-                board[i][j] += Math.max(board[i-1][j], board[i][j-1]);
                 if (doc1.charAt(i-1) == doc2.charAt(j-1) && i >= board[i][j] && j >= board[i][j]) {
-                    board[i][j] += 1;
+                    board[i][j] = board[i-1][j-1] + 1;
+                } else {
+                    board[i][j] = Math.max(board[i-1][j], board[i][j-1]);
                 }
             }
         }
+
+        String oneString = "";
+
+        int i = doc1.length();
+        int j = doc2.length();
+        while (i != 0 && j != 0) {
+
+        }
+
         return board[doc1.length()][doc2.length()];
-//        return recursive(doc2, doc1, 0, 0, memoizationBoard);
-    }
-
-    public static int recursive(String text1, String text2, int idx1, int idx2, int[][] memoizationBoard) {
-        if (idx1 >= text1.length() || idx2 >= text2.length()) {
-            return 1;
-        }
-
-        int maxLen = Integer.MIN_VALUE;
-        for (int i = idx1; i < text1.length(); i++) {
-            if (text1.charAt(i) == text2.charAt(idx2)) {
-                int newLen;
-                if (memoizationBoard[i][idx2] != 0) {
-                    newLen = memoizationBoard[i][idx2];
-                }
-                else {
-                    newLen = 1 + recursive(text1, text2, i + 1, idx2 + 1, memoizationBoard);
-                    memoizationBoard[i][idx2] = newLen;
-                }
-                maxLen = Math.max(maxLen, newLen);
-            }
-        }
-
-        return maxLen;
     }
 }
